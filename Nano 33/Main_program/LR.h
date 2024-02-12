bd6d738020c6003e3166f192e446edaeae245031
txol <106444980+txol@users.noreply.github.com>
2024-02-12 14:55:36 +0100

    Add files via upload

---
+++ Nano 33/Main_program/LR.h
@@ -0,0 +1,47 @@
+#ifndef LOGISTIC_REGRESSION_HEADER_H
+#define LOGISTIC_REGRESSION_HEADER_H
+
+#include <Arduino.h>
+#include <algorithm>
+#include <climits>
+
+struct findHighestValues {
+  void operator()(int8_t arr[], int8_t size, int8_t& firstHighest, int8_t& secondHighest);
+};
+
+const float weight1 = 0.03105428;
+const float weight2 = -0.00686018;
+const float beta = -3.42381517;
+int8_t value1;
+int8_t value2;
+
+int LRpredict(int8_t scores[]) {
+  findHighestValues()(scores, 10, value1, value2);
+
+  float weighted_sum = beta + weight1 * value1 + weight2 * value2;
+  float probability = 1.0 / (1.0 + exp(-weighted_sum));
+
+  if (probability >= 0.5) {
+    return 1; // Classify as positive
+  } else {
+    return 0; // Classify as negative
+  }
+}
+
+void findHighestValues::operator()(int8_t arr[], int8_t size, int8_t& firstHighest, int8_t& secondHighest) {
+  // Initialize the highest and second highest values
+  firstHighest = INT8_MIN;
+  secondHighest = INT8_MIN;
+
+  // Traverse the array to find the two highest values
+  for (int i = 0; i < size; i++) {
+    if (arr[i] > firstHighest) {
+      secondHighest = firstHighest;
+      firstHighest = arr[i];
+    } else if (arr[i] > secondHighest && arr[i] < firstHighest) {
+      secondHighest = arr[i];
+    }
+  }
+}
+
+#endif
